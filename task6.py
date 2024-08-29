77785517bc87dda560ca9c1e55bd7ed648f14dfa
Alexander <alexaander.yudin@gmail.com>
2024-08-29 22:53:57 +0500

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,5 @@
+CompletedHomeworkCount = 12
+NumberOfHoursSpent = 1.5
+CourseName = 'Python'
+AverageExecutionTime = NumberOfHoursSpent/CompletedHomeworkCount
+print ('Курс:',CourseName,",",'всего задач:',CompletedHomeworkCount,',','затрачено часов:',NumberOfHoursSpent,',','среднее время выполнения',NumberOfHoursSpent/CompletedHomeworkCount,'.')
